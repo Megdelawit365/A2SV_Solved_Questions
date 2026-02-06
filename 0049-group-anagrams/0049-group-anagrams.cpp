6b70e4b07b27d22b250c78bfd9b9a15d136142bc
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-02-06 22:11:14 +0300

    Time: 14 ms (74.44%), Space: 25 MB (78.11%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       unordered_map<string, vector<string>> map;
+       for(string s:strs){
+        string t = s;
+        sort(t.begin(),t.end());
+        map[t].push_back(s);
+       }
+       vector<vector<string>> output;
+       for(auto& m:map){
+        output.push_back(m.second);
+       }
+       return output;
+    }
+};
