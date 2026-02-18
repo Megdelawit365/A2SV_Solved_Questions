8bd6d0a889962625513623b573920f739231bea5
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-02-18 18:36:01 +0300

    Time: 428 ms (9.23%), Space: 19.6 MB (73.5%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,10 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        hashmap = {}
-        for i in range(len(names)):
-            hashmap[heights[i]] = names[i]
         for i in range(len(names)):
             for j in range(i,len(names)):
                 if heights[i] < heights[j]:
                     heights[i], heights[j] = heights[j], heights[i]
-        ans = []
-        for h in heights:
-            ans.append(hashmap[h])
-        return ans
+                    names[i], names[j] = names[j], names[i]
+        
+        return names
         
