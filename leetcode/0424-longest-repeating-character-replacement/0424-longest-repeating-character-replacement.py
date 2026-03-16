1e9eb30a6475e83bebe4accd8f474b65e80bf1a5
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-03-16 16:51:29 +0300

    Add solution for Longest Repeating Character Replacement

---
+++ leetcode/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,18 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxLen = 0
+        maxCount = 0
+        count = defaultdict(int)
+        i,j = 0,0
+        while j < len(s):
+            count[s[j]] += 1
+            maxCount = max(maxCount, count[s[j]])
+            while (len(count) > k+1 or sum(count.values()) - maxCount > k) and i < len(s):
+                count[s[i]] -= 1
+                if count[s[i]] == 0:
+                    del count[s[i]]
+                i += 1
+                
+            maxLen = max(maxLen, j - i + 1)
+            j += 1
+        return maxLen
