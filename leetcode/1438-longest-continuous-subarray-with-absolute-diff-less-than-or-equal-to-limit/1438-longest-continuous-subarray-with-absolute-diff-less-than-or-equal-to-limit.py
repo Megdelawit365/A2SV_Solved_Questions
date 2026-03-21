5f0348bdf63be46a9b1f9d9c7af640ae7627ef4c
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-03-21 09:40:36 +0300

    Add solution for Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- leetcode/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ leetcode/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,25 +1,28 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        maxSize = 0
-        maxD = deque()
-        minD = deque()
+        _min = deque()
+        _max = deque()
+        l,r = 0, 0
+        ans = 0
+        
 
-        left = 0
-        for right in range(len(nums)):
-            while maxD and nums[right] > maxD[-1]:
-                maxD.pop()
-            maxD.append(nums[right])
+        while r < len(nums):
+            while _min and _min[-1] > nums[r]:
+                _min.pop()
+            _min.append(nums[r])
 
-            while minD and nums[right] < minD[-1]:
-                minD.pop()
-            minD.append(nums[right])
+            while _max and _max[-1] < nums[r]:
+                _max.pop()
+            _max.append(nums[r])
 
-            while maxD[0] - minD[0] > limit:
-                if nums[left] == maxD[0]:
-                    maxD.popleft()
-                if nums[left] == minD[0]:
-                    minD.popleft()
-                left += 1
-            maxSize = max(maxSize, right - left + 1)
+            while _max[0] - _min[0] > limit:
+                if nums[l] == _max[0]:
+                    _max.popleft()
+                if nums[l] == _min[0]:
+                    _min.popleft()
+                l += 1
             
-        return maxSize
+            ans = max(ans, r - l + 1)
+            r += 1
+        return ans
+            
