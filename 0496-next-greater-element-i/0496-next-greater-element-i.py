673bd86326ef3acca591be0b6a2642d4a58cc1e3
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-03-21 09:14:26 +0300

    Time: 0 ms (100%), Space: 19.2 MB (97.27%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,27 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    #     1-0
-    #     3-1
-    #     4-2
-    #     2-3
-        
-    # 1 3 4 2
-    #     4  -1
-    # 1 5 2 3 7
-    # 0 0 0 7 0
-        prefix = [0] * (len(nums2) + 1)
-        prefix[-1] = -1
-        for i in range(len(nums2)-1,0,-1):
-            if nums2[i] > nums2[i-1]:
-                prefix[i] = nums2[i]
-            else:
-                prefix[i] = prefix[i+1]
-        count = {}
-        for i,n in enumerate(nums2):
-            count[n] = i
+        count = defaultdict(lambda:-1)
+        stack = []
+        for i,num in enumerate(nums2):
+            while stack and nums2[stack[-1]] < num:
+                count[nums2[stack.pop()]] = nums2[i]
+            stack.append(i)
         ans = []
-        for n in nums1:
-            idx = count[n]
-            ans.append(prefix[idx+1])
-
-        return ans
+        for i in range(len(nums1)):
+            ans.append(count[nums1[i]])
+        return(ans)
