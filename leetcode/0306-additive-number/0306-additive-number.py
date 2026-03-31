bfe2a89bdea9c9aada2ca7f135f084c980106c5f
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-03-31 13:46:23 +0300

    Add solution for Additive Number

---
+++ leetcode/0306-additive-number/0306-additive-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        def split(i,path):
+            if i == len(num):
+                for j in range(len(path)-1,1,-1):
+                    if path[j] != path[j-1] + path[j-2]:
+                        return False
+                return len(path) >= 3
+
+            for j in range(i,len(num)):
+                if num[i] == '0' and j > i:
+                    break
+                val = int(num[i:j+1])
+                path.append(val)
+                
+                if split(j+1,path):
+                    return True
+                path.pop()
+            return False
+        return split(0,[])
