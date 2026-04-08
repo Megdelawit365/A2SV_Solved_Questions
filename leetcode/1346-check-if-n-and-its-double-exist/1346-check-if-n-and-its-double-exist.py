f896bf0eab4b91ab1af219ab81ddefcbbab5b54f
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-04-08 19:31:04 +0300

    Add solution for Check If N and Its Double Exist

---
+++ leetcode/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        seen = set()
+        for a in arr:
+            if a*2 in seen or a/2 in seen:
+                return True
+            seen.add(a)
+        
+        return False
