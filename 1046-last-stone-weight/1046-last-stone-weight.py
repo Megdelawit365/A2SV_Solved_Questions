6bdc9370201971ac8b0e79cad67244b005b57043
Megdelawit Aynalem <megdelawitaynalem365@gmail.com>
2026-05-18 17:52:44 +0300

    Time: 3 ms (11.15%), Space: 19.3 MB (91.09%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap = [-s for s in stones]
+        heapq.heapify(heap)
+
+        while len(heap) > 1:
+            first = -heapq.heappop(heap)
+            second = -heapq.heappop(heap)
+
+            if first != second:
+                heapq.heappush(heap,-(first - second))
+    
+        return -heap[0] if heap else 0
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
